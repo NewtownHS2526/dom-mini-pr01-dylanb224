5998fa97329392d779ed07e877a21a7b0af440cb
Dylan BarreraRamirez <dylanb224@nycstudents.net>
2026-01-13 14:13:17 -0500

    done

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ let brown_position = 1;
 // Query Selectors Here
 const blueButton = document.querySelector("#blue-button");
 const blueHorse = document.querySelector("#blue-horse");
+const pinkButton = document.querySelector("#pink-button");
+const pinkHorse = document.querySelector("#pink-horse"); 
+const brownButton = document.querySelector("#brown-button");
+const brownHorse = document.querySelector("#brown-horse");
 
 
 
@@ -19,13 +23,21 @@ const advanceBlue = (e) => {
   changePosition(blueHorse, blue_position);
 };
 const advancePink = (e) => {
-
+  pink_position += 1;
+  changePosition(pinkHorse, pink_position);
+};
+const advanceBrown = (e) => {
+  brown_position += 1;
+  changePosition(brownHorse, brown_position)
 };
 
 
 
 // Check for a winner
 const checkWinner = (position, color) => {
+  if (position > 5) {
+    alert(`${color} horse wins!`);
+  }
 
 };
 
@@ -33,3 +45,5 @@ const checkWinner = (position, color) => {
 
 // Event Listeners Here
 blueButton.addEventListener("click", advanceBlue);
+pinkButton.addEventListener("click", advancePink);
+brownButton.addEventListener("click", advanceBrown);
